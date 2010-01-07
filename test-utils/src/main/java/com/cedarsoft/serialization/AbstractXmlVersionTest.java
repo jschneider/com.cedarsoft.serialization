ea63503c9bb1cc9ff6882f23db8642f192129fb9
Johannes Schneider <js@cedarsoft.com>
2010-01-07 15:45:47 +0100

    version test upgraded to use namespaces

--- test-utils/src/main/java/com/cedarsoft/serialization/AbstractXmlVersionTest.java
+++ test-utils/src/main/java/com/cedarsoft/serialization/AbstractXmlVersionTest.java
@@ -1,9 +1,15 @@
 package com.cedarsoft.serialization;
 
 import com.cedarsoft.Version;
+import org.jdom.Document;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +19,7 @@ import java.util.Map;
 public abstract class AbstractXmlVersionTest<T> extends AbstractVersionTest<T> {
   @NotNull
   @Override
-  protected final Map<? extends Version, ? extends byte[]> getSerialized() {
+  protected final Map<? extends Version, ? extends byte[]> getSerialized() throws Exception {
     Map<Version, byte[]> serializedMap = new HashMap<Version, byte[]>();
     for ( Map.Entry<? extends Version, ? extends String> entry : getSerializedXml().entrySet() ) {
       byte[] xml = processXml( entry.getValue(), entry.getKey() );
@@ -32,9 +38,16 @@ public abstract class AbstractXmlVersionTest<T> extends AbstractVersionTest<T> {
    * @return the byte array using the xml string
    */
   @NotNull
-  protected byte[] processXml( @NotNull @NonNls final String xml, @NotNull Version version ) {
-    return ( "<?" + AbstractXmlSerializer.PI_TARGET_FORMAT + " " + version.toString() + "?>" + xml ).getBytes();
+  protected byte[] processXml( @NotNull @NonNls final String xml, @NotNull Version version ) throws Exception {
+    String nameSpace = ( ( AbstractXmlSerializer<?, ?, ?, ?> ) getSerializer() ).createNameSpaceUri( version );
+
+    Document doc = new SAXBuilder().build( new ByteArrayInputStream( xml.getBytes() ) );
+    doc.getRootElement().setNamespace( Namespace.getNamespace( nameSpace ) );
+
+    return new XMLOutputter( Format.getPrettyFormat() ).outputString( doc ).getBytes();
+    //    return ( "<?" + AbstractXmlSerializer.PI_TARGET_FORMAT + " " + version.toString() + "?>" + xml ).getBytes();
   }
+
 
   /**
    * Returns a map containing the serialized xmls
