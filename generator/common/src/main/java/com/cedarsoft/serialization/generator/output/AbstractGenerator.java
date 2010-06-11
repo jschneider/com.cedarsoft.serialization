b636743093c7bf76e58309999eacb6fb8cb0b7c4
Johannes Schneider <js@cedarsoft.com>
2010-06-11 12:19:49 +0200

    Generator: Moved common generation stuff up

--- generator/common/src/main/java/com/cedarsoft/serialization/generator/output/AbstractGenerator.java
+++ generator/common/src/main/java/com/cedarsoft/serialization/generator/output/AbstractGenerator.java
@@ -1,12 +1,20 @@
 package com.cedarsoft.serialization.generator.output;
 
+import com.cedarsoft.Version;
+import com.cedarsoft.VersionException;
 import com.cedarsoft.serialization.generator.decision.DecisionCallback;
 import com.cedarsoft.serialization.generator.model.DomainObjectDescriptor;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 /**
  * @param <T> the type of the decision callback
@@ -77,4 +85,81 @@ public abstract class AbstractGenerator<T extends DecisionCallback> {
   public CodeGenerator<T> getCodeGenerator() {
     return codeGenerator;
   }
+
+  public void generate( @NotNull DomainObjectDescriptor domainObjectDescriptor ) throws JClassAlreadyExistsException {
+    JClass domainType = codeModel.ref( domainObjectDescriptor.getQualifiedName() );
+
+    //the class
+    JDefinedClass serializerClass = codeModel._class( createSerializerClassName( domainType.fullName() ) )._extends( createSerializerExtendsExpression( domainType ) );
+
+    //the constructor
+    createConstructor( serializerClass, domainObjectDescriptor );
+
+    JMethod serializeMethod = createSerializeMethodStub( domainType, serializerClass );
+    JMethod deserializeMethod = createDeserializeMethodStub( domainType, serializerClass );
+
+    //Add the serialize stuff
+    addSerializationStuff( domainObjectDescriptor, serializeMethod, deserializeMethod );
+  }
+
+  /**
+   * Creates the constructor for the given serializer class (if necessary)
+   *
+   * @param serializerClass        the serialize class
+   * @param domainObjectDescriptor the domain object descriptor
+   */
+  protected abstract void createConstructor( @NotNull JDefinedClass serializerClass, @NotNull DomainObjectDescriptor domainObjectDescriptor );
+
+  @NotNull
+  protected JMethod createDeserializeMethodStub( @NotNull JType domainType, @NotNull JDefinedClass serializerClass ) {
+    JMethod deserializeMethod = serializerClass.method( JMod.PUBLIC, domainType, METHOD_NAME_DESERIALIZE );
+    deserializeMethod.param( getSerializeFromType(), "deserializeFrom" ).annotate( NotNull.class );
+    deserializeMethod.param( Version.class, "formatVersion" ).annotate( NotNull.class );
+    deserializeMethod.annotate( Override.class );
+    deserializeMethod._throws( IOException.class )._throws( VersionException.class )._throws( getExceptionType() );
+    return deserializeMethod;
+  }
+
+  @NotNull
+  protected JMethod createSerializeMethodStub( @NotNull JType domainType, @NotNull JDefinedClass serializerClass ) {
+    JMethod serializeMethod = serializerClass.method( JMod.PUBLIC, Void.TYPE, METHOD_NAME_SERIALIZE );
+    serializeMethod.annotate( Override.class );
+    serializeMethod.param( getSerializeToType(), "serializeTo" ).annotate( NotNull.class );
+    serializeMethod.param( domainType, "object" ).annotate( NotNull.class );
+    serializeMethod._throws( IOException.class )._throws( getExceptionType() );
+    return serializeMethod;
+  }
+
+  /**
+   * Adds the serialization stuff to the (de)serialize methods
+   *
+   * @param domainObjectDescriptor the domain object descriptor
+   * @param serializeMethod        the serialize method
+   * @param deserializeMethod      the deserialize method
+   */
+  protected abstract void addSerializationStuff( @NotNull DomainObjectDescriptor domainObjectDescriptor, @NotNull JMethod serializeMethod, @NotNull JMethod deserializeMethod );
+
+  /**
+   * Returns the exception type that is thrown on the serialize and deserialize methods
+   *
+   * @return the exception type
+   */
+  @NotNull
+  protected abstract Class<?> getExceptionType();
+
+  /**
+   * Returns the type of the serialize from object
+   *
+   * @return the type of the serialize from object
+   */
+  @NotNull
+  protected abstract Class<?> getSerializeFromType();
+
+  /**
+   * Returns the type of the serializeTo type
+   *
+   * @return the type of the serializeTo type
+   */
+  @NotNull
+  protected abstract Class<?> getSerializeToType();
 }
--- generator/common/src/main/java/com/cedarsoft/serialization/generator/output/AbstractXmlGenerator.java
+++ generator/common/src/main/java/com/cedarsoft/serialization/generator/output/AbstractXmlGenerator.java
@@ -1,23 +1,17 @@
 package com.cedarsoft.serialization.generator.output;
 
 import com.cedarsoft.Version;
-import com.cedarsoft.VersionException;
 import com.cedarsoft.VersionRange;
 import com.cedarsoft.serialization.NameSpaceSupport;
 import com.cedarsoft.serialization.generator.decision.XmlDecisionCallback;
 import com.cedarsoft.serialization.generator.model.DomainObjectDescriptor;
 import com.sun.codemodel.JClass;
-import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JExpr;
 import com.sun.codemodel.JInvocation;
-import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
-import com.sun.codemodel.JType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 
 /**
  *
@@ -40,68 +34,11 @@ public abstract class AbstractXmlGenerator extends AbstractGenerator<XmlDecision
     super( codeGenerator );
   }
 
-  public void generate( @NotNull DomainObjectDescriptor domainObjectDescriptor ) throws JClassAlreadyExistsException {
-    JClass domainType = codeModel.ref( domainObjectDescriptor.getQualifiedName() );
-
-    //the class
-    JDefinedClass serializerClass = codeModel._class( createSerializerClassName( domainType.fullName() ) )._extends( createSerializerExtendsExpression( domainType ) );
-
-    //the constructor
+  @Override
+  protected void createConstructor( @NotNull JDefinedClass serializerClass, @NotNull DomainObjectDescriptor domainObjectDescriptor ) {
     serializerClass.constructor( JMod.PUBLIC ).body()
       .invoke( "super" ).arg( getDefaultElementName( domainObjectDescriptor ) ).arg( getNamespace( domainObjectDescriptor ) )
-      .arg( createDefaultVersionRangeInvocation( VERSION, VERSION ) );
-
-    JMethod serializeMethod = createSerializeMethodStub( domainType, serializerClass );
-    JMethod deserializeMethod = createDeserializeMethodStub( domainType, serializerClass );
-
-    //Add the serialize stuff
-    addSerializationStuff( domainObjectDescriptor, serializeMethod, deserializeMethod );
-  }
-
-  @NotNull
-  protected JMethod createDeserializeMethodStub( @NotNull JType domainType, @NotNull JDefinedClass serializerClass ) {
-    JMethod deserializeMethod = serializerClass.method( JMod.PUBLIC, domainType, METHOD_NAME_DESERIALIZE );
-    deserializeMethod.param( getSerializeFromType(), "deserializeFrom" ).annotate( NotNull.class );
-    deserializeMethod.param( Version.class, "formatVersion" ).annotate( NotNull.class );
-    deserializeMethod.annotate( Override.class );
-    deserializeMethod._throws( IOException.class )._throws( VersionException.class )._throws( getExceptionType() );
-    return deserializeMethod;
-  }
-
-  protected abstract void addSerializationStuff( @NotNull DomainObjectDescriptor domainObjectDescriptor, @NotNull JMethod serializeMethod, @NotNull JMethod deserializeMethod );
-
-  /**
-   * Returns the exception type that is thrown on the serialize and deserialize methods
-   *
-   * @return the exception type
-   */
-  @NotNull
-  protected abstract Class<?> getExceptionType();
-
-  /**
-   * Returns the type of the serialize from object
-   *
-   * @return the type of the serialize from object
-   */
-  @NotNull
-  protected abstract Class<?> getSerializeFromType();
-
-  /**
-   * Returns the type of the serializeTo type
-   *
-   * @return the type of the serializeTo type
-   */
-  @NotNull
-  protected abstract Class<?> getSerializeToType();
-
-  @NotNull
-  protected JMethod createSerializeMethodStub( @NotNull JType domainType, @NotNull JDefinedClass serializerClass ) {
-    JMethod serializeMethod = serializerClass.method( JMod.PUBLIC, Void.TYPE, METHOD_NAME_SERIALIZE );
-    serializeMethod.annotate( Override.class );
-    serializeMethod.param( getSerializeToType(), "serializeTo" ).annotate( NotNull.class );
-    serializeMethod.param( domainType, "object" ).annotate( NotNull.class );
-    serializeMethod._throws( IOException.class )._throws( getExceptionType() );
-    return serializeMethod;
+      .arg( createDefaultVersionRangeInvocation( AbstractXmlGenerator.VERSION, AbstractXmlGenerator.VERSION ) );
   }
 
   @NotNull
