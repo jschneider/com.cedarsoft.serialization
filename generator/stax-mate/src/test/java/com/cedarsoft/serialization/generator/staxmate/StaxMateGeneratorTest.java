f2cc3ba8a1a9dff01478713649b7e1bdca49c8e1
Johannes Schneider <js@cedarsoft.com>
2010-06-11 14:24:34 +0200

    Generator: Fixed test

--- generator/stax-mate/src/test/java/com/cedarsoft/serialization/generator/staxmate/StaxMateGeneratorTest.java
+++ generator/stax-mate/src/test/java/com/cedarsoft/serialization/generator/staxmate/StaxMateGeneratorTest.java
@@ -59,6 +59,74 @@ public class StaxMateGeneratorTest {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     model.build( new SingleStreamCodeWriter( out ) );
 
-    assertEquals( out.toString(), "asdf" );
+    assertEquals( out.toString().trim(), "-----------------------------------com.cedarsoft.serialization.generator.staxmate.test.WindowSerializer.java-----------------------------------\n" +
+      "\n" +
+      "package com.cedarsoft.serialization.generator.staxmate.test;\n" +
+      "\n" +
+      "import java.io.IOException;\n" +
+      "import javax.xml.stream.XMLStreamException;\n" +
+      "import javax.xml.stream.XMLStreamReader;\n" +
+      "import com.cedarsoft.Version;\n" +
+      "import com.cedarsoft.VersionException;\n" +
+      "import com.cedarsoft.VersionRange;\n" +
+      "import com.cedarsoft.serialization.stax.AbstractStaxMateSerializer;\n" +
+      "import org.codehaus.staxmate.out.SMOutputElement;\n" +
+      "import org.jetbrains.annotations.NotNull;\n" +
+      "\n" +
+      "public class WindowSerializer\n" +
+      "    extends AbstractStaxMateSerializer<Window>\n" +
+      "{\n" +
+      "\n" +
+      "\n" +
+      "    public WindowSerializer() {\n" +
+      "        super(\"window\", \"http://www.cedarsoft.com/serialization/generator/staxmate/test/Window/1.0.0\", VersionRange.from(1, 0, 0).to(1, 0, 0));\n" +
+      "    }\n" +
+      "\n" +
+      "    @Override\n" +
+      "    public void serialize(\n" +
+      "        @NotNull\n" +
+      "        SMOutputElement serializeTo,\n" +
+      "        @NotNull\n" +
+      "        Window object)\n" +
+      "        throws IOException, XMLStreamException\n" +
+      "    {\n" +
+      "        //width\n" +
+      "        serializeTo.addAttribute(\"width\", String.valueOf(object.getWidth()));\n" +
+      "        //height\n" +
+      "        serializeTo.addAttribute(\"height\", String.valueOf(object.getHeight()));\n" +
+      "        //description\n" +
+      "        serializeTo.addElementWithCharacters(serializeTo.getNamespace(), \"description\", object.getDescription());\n" +
+      "        //anInt\n" +
+      "        serializeTo.addElementWithCharacters(serializeTo.getNamespace(), \"anInt\", String.valueOf(object.getAnInt()));\n" +
+      "        //floatField\n" +
+      "        serializeTo.addElementWithCharacters(serializeTo.getNamespace(), \"floatField\", String.valueOf(object.getFloatField()));\n" +
+      "    }\n" +
+      "\n" +
+      "    @Override\n" +
+      "    public Window deserialize(\n" +
+      "        @NotNull\n" +
+      "        XMLStreamReader deserializeFrom,\n" +
+      "        @NotNull\n" +
+      "        Version formatVersion)\n" +
+      "        throws VersionException, IOException, XMLStreamException\n" +
+      "    {\n" +
+      "        //width\n" +
+      "        double width = Double.parseDouble(deserializeFrom.getAttributeValue(null, \"width\"));\n" +
+      "        //height\n" +
+      "        int height = Integer.parseInt(deserializeFrom.getAttributeValue(null, \"height\"));\n" +
+      "        //description\n" +
+      "        String description = getChildText(deserializeFrom, \"description\");\n" +
+      "        //anInt\n" +
+      "        Integer anInt = Integer.parseInt(getChildText(deserializeFrom, \"anInt\"));\n" +
+      "        //floatField\n" +
+      "        float floatField = Float.parseFloat(getChildText(deserializeFrom, \"floatField\"));\n" +
+      "        closeTag(deserializeFrom);\n" +
+      "        //Constructing the deserialized object\n" +
+      "        Window object = new Window(description, width, height, anInt);\n" +
+      "        object.setFloatField(floatField);\n" +
+      "        return object;\n" +
+      "    }\n" +
+      "\n" +
+      "}" );
   }
 }
