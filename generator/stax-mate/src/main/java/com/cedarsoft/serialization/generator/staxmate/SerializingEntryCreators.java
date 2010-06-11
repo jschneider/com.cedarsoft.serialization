1e3745381b5899cc5a80077674046e4ef50d098a
Johannes Schneider <js@cedarsoft.com>
2010-06-11 14:54:09 +0200

    cosmetic: added missing type parameter

--- generator/stax-mate/src/main/java/com/cedarsoft/serialization/generator/staxmate/SerializingEntryCreators.java
+++ generator/stax-mate/src/main/java/com/cedarsoft/serialization/generator/staxmate/SerializingEntryCreators.java
@@ -1,5 +1,6 @@
 package com.cedarsoft.serialization.generator.staxmate;
 
+import com.cedarsoft.serialization.generator.decision.XmlDecisionCallback;
 import com.cedarsoft.serialization.generator.output.CodeGenerator;
 import com.cedarsoft.serialization.generator.output.ParseExpressionFactory;
 import com.sun.codemodel.JCodeModel;
@@ -12,7 +13,7 @@ public class SerializingEntryCreators {
   @NotNull
   private final SerializingEntryGenerator toStringEntryGenerator;
 
-  public SerializingEntryCreators( @NotNull CodeGenerator codeGenerator ) {
+  public SerializingEntryCreators( @NotNull CodeGenerator<XmlDecisionCallback> codeGenerator ) {
     toStringEntryGenerator = new ToStringSerializingEntryGenerator( codeGenerator );
   }
 
