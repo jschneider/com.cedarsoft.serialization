422304ef8ade68e6c27490c9420727908804ca36
Johannes Schneider <js@cedarsoft.com>
2010-07-10 22:42:14 +0200

    javadoc

--- test-utils/src/main/java/com/cedarsoft/serialization/AbstractXmlSerializerTest2.java
+++ test-utils/src/main/java/com/cedarsoft/serialization/AbstractXmlSerializerTest2.java
@@ -51,7 +51,7 @@ import java.io.IOException;
  * <p/>
  * Attention: it is necessary to define at least one DataPoint:
  * <p/>
- * <pre>&#064;DataPoint<br/>public static final Entry&lt;T&gt; entry1 = create(<br/> new DomainObject(),<br/> &quot;&lt;xml/&gt;&quot; );</pre>
+ * <pre>&#064;DataPoint<br/>public static final Entry&lt;T&gt; ENTRY1 = create(<br/> new DomainObject(),<br/> &quot;&lt;xml/&gt;&quot; );</pre>
  *
  * @param <T> the type of the serialized object
  */
