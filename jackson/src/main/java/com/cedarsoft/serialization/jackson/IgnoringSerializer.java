ed538292c518686ec54407485202ee1d14e4c111
Johannes Schneider <js@cedarsoft.com>
2012-01-09 21:37:48 +0100

    ignores one object/array

---
+++ jackson/src/main/java/com/cedarsoft/serialization/jackson/IgnoringSerializer.java
@@ -0,0 +1,61 @@
+package com.cedarsoft.serialization.jackson;
+
+import com.cedarsoft.version.Version;
+import com.cedarsoft.version.VersionException;
+import com.cedarsoft.version.VersionRange;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.JsonToken;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+/**
+ * Simply ignores the object/array
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class IgnoringSerializer extends AbstractJacksonSerializer<Void> {
+  public IgnoringSerializer() {
+    super( "ignoring", VersionRange.single( 0, 0, 0 ) );
+  }
+
+  @Override
+  public void serialize( @Nonnull JsonGenerator serializeTo, @Nonnull Void object, @Nonnull Version formatVersion ) throws IOException, VersionException, JsonProcessingException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Nonnull
+  @Override
+  public Void deserialize( @Nonnull JsonParser deserializeFrom, @Nonnull Version formatVersion ) throws IOException, VersionException, JsonProcessingException {
+    JsonToken inToken = deserializeFrom.nextToken();
+    JsonToken outToken = findOutToken( inToken );
+
+    int depth = 1;
+
+    while ( depth > 0 ) {
+      JsonToken next = deserializeFrom.nextToken();
+      if ( next == inToken ) {
+        depth++;
+      }
+      if ( next == outToken ) {
+        depth--;
+      }
+    }
+
+    //noinspection ConstantConditions
+    return null;
+  }
+
+  @Nonnull
+  private JsonToken findOutToken( @Nonnull JsonToken inToken ) {
+    switch ( inToken ) {
+      case START_OBJECT:
+        return JsonToken.END_OBJECT;
+      case START_ARRAY:
+        return JsonToken.END_ARRAY;
+    }
+
+    throw new IllegalArgumentException( "No end token found for <" + inToken + ">" );
+  }
+}
