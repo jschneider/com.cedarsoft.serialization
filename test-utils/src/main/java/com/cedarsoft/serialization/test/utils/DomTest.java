1f16783ad7ac5318fdcbc8f293668cad63c9a3e3
Johannes Schneider <js@cedarsoft.com>
2012-01-25 11:45:25 +0100

    fixed test

--- test-utils/src/main/java/com/cedarsoft/serialization/test/utils/DomTest.java
+++ test-utils/src/main/java/com/cedarsoft/serialization/test/utils/DomTest.java
@@ -6,7 +6,6 @@ import org.junit.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import javax.annotation.Nonnull;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.ByteArrayInputStream;
@@ -18,16 +17,13 @@ import static org.fest.assertions.Assertions.assertThat;
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
 public class DomTest {
-  @Nonnull
-  byte[] xml = "<a/>".getBytes();
-
   @Test
   public void testIt() throws Exception {
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     factory.setNamespaceAware( true );
     DocumentBuilder documentBuilder = factory.newDocumentBuilder();
 
-    Document doc = documentBuilder.parse( new ByteArrayInputStream( xml ) );
+    Document doc = documentBuilder.parse( new ByteArrayInputStream( "<a/>".getBytes() ) );
 
     Element element = doc.getDocumentElement();
     assertThat( element ).isNotNull();
@@ -41,25 +37,18 @@ public class DomTest {
     element.appendChild( doc.createElementNS( "manuallyChangedChildNS", "DaNewChild" ) );
     element.appendChild( doc.createElement( "child2WithoutNS" ) );
 
-
     new XmlNamespaceTranslator()
-      .addTranslation( null, "MyNS" )
-//      .addTranslation( "", "MyNS" )
-      .translateNamespaces( doc, false );
-
-
-//    Element copy = doc.createElementNS( "dans", element.getTagName() );
-//    NamedNodeMap attributes = element.getAttributes();
-//    for ( int i = 0; i < attributes.getLength(); i++ ) {
-//      Node item = attributes.item( i );
-//      copy.setAttributeNodeNS(  )
-//    }
-
+        .addTranslation( null, "MyNS" )
+        .translateNamespaces( doc, false );
 
     StringWriter out = new StringWriter();
     XmlCommons.out( doc, out );
 
-    assertThat( out.toString() ).isEqualTo( "asdf" );
+    assertThat( out.toString() ).isEqualTo( "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                                                "<a daAttr=\"daval\" xmlns=\"MyNS\">\n" +
+                                                "  <DaNewChild xmlns=\"manuallyChangedChildNS\"/>\n" +
+                                                "  <child2WithoutNS/>\n" +
+                                                "</a>\n" );
   }
 
 }
