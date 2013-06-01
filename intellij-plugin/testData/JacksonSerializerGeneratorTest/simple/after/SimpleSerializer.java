8917f3fcff7a7735bcd2ced45b815ea8cbb16ec0
Johannes Schneider <js@cedarsoft.com>
2013-06-01 22:14:51 +0200

    updated test

--- intellij-plugin/testData/JacksonSerializerGeneratorTest/simple/after/SimpleSerializer.java
+++ intellij-plugin/testData/JacksonSerializerGeneratorTest/simple/after/SimpleSerializer.java
@@ -1,7 +1,17 @@
 public class SimpleSerializer extends com.cedarsoft.serialization.jackson.AbstractJacksonSerializer<Simple> {
-    public SimpleSerializer(StringSerializer stringSerializer) {
+    public SimpleSerializer(@org.jetbrains.annotations.NotNull StringSerializer stringSerializer) {
         super("simple_serializer", com.cedarsoft.version.VersionRange.from(1, 0, 0).to());
         getDelegatesMappings().add(stringSerializer).responsibleFor(String.class).map(1, 0, 0).toDelegateVersion(1, 0, 0);
         assert getDelegatesMappings().verify();
+    }
+
+    @Override
+    public void serialize(@org.jetbrains.annotations.NotNull com.fasterxml.jackson.core.JsonGenerator serializeTo, @org.jetbrains.annotations.NotNull Simple object, @org.jetbrains.annotations.NotNull com.cedarsoft.version.Version formatVersion) {
+        verifyVersionWritable(formatVersion);
+    }
+
+    @Override
+    public void deserialize(@org.jetbrains.annotations.NotNull com.fasterxml.jackson.core.JsonParser deserializeFrom, @org.jetbrains.annotations.NotNull com.cedarsoft.version.Version formatVersion) {
+        verifyVersionWritable(formatVersion);
     }
 }
