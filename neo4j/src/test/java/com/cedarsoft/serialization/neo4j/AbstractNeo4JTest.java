d79807736f9cb7f55ef025116a4578da6ac6ab7d
Johannes Schneider <js@cedarsoft.com>
2013-10-04 12:00:28 +0200

    added TemporaryFolder rule

--- neo4j/src/test/java/com/cedarsoft/serialization/neo4j/AbstractNeo4JTest.java
+++ neo4j/src/test/java/com/cedarsoft/serialization/neo4j/AbstractNeo4JTest.java
@@ -1,8 +1,11 @@
 package com.cedarsoft.serialization.neo4j;
 
 import org.junit.*;
+import org.junit.rules.*;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.test.TestGraphDatabaseFactory;
+
+import java.io.IOException;
 
 /**
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
@@ -10,9 +13,12 @@ import org.neo4j.test.TestGraphDatabaseFactory;
 public class AbstractNeo4JTest {
   protected GraphDatabaseService graphDb;
 
+  @Rule
+  public TemporaryFolder tmp = new TemporaryFolder();
+
   @Before
-  public void prepareTestDatabase() {
-    graphDb = new TestGraphDatabaseFactory().newImpermanentDatabase();
+  public void prepareTestDatabase() throws IOException {
+    graphDb = new TestGraphDatabaseFactory().newImpermanentDatabase( tmp.newFolder().getAbsolutePath() );
   }
 
   @After
