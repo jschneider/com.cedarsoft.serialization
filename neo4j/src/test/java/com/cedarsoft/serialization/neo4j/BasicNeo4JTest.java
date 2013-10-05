ddf3e1ca8201790c108f233715c442e55cea4613
Johannes Schneider <js@cedarsoft.com>
2013-10-05 11:56:06 +0200

    added tests

--- neo4j/src/test/java/com/cedarsoft/serialization/neo4j/BasicNeo4JTest.java
+++ neo4j/src/test/java/com/cedarsoft/serialization/neo4j/BasicNeo4JTest.java
@@ -1,9 +1,19 @@
 package com.cedarsoft.serialization.neo4j;
 
-import com.cedarsoft.serialization.neo4j.AbstractNeo4JTest;
+import com.cedarsoft.serialization.neo4j.test.Relations;
 import org.junit.*;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.event.TransactionData;
+import org.neo4j.graphdb.event.TransactionEventHandler;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.jmx.JmxUtils;
+import org.neo4j.kernel.GraphDatabaseAPI;
+
+import javax.management.ObjectName;
+
+import java.util.Date;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -11,6 +21,108 @@ import static org.fest.assertions.Assertions.assertThat;
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
 public class BasicNeo4JTest extends AbstractNeo4JTest {
+  @Test
+  public void testJmx() throws Exception {
+    ObjectName objectName = JmxUtils.getObjectName( ( GraphDatabaseAPI ) graphDb, "Kernel" );
+    String version = JmxUtils.getAttribute( objectName, "KernelVersion" );
+
+    assertThat( version ).isEqualTo( "Neo4j - Graph Database Kernel 2.0.0-M05" );
+  }
+
+  @Test
+  public void testLegacyIndex() throws Exception {
+    graphDb.registerTransactionEventHandler( new TransactionEventHandler<Object>() {
+      @Override
+      public Object beforeCommit( TransactionData data ) throws Exception {
+        System.out.println( "com.cedarsoft.serialization.neo4j.BasicNeo4JTest.beforeCommit" );
+        System.out.println( data );
+        System.out.println( "Created Nodes: " + data.createdNodes() );
+        System.out.println( "Created Relationships: " + data.createdRelationships() );
+        return null;
+      }
+
+      @Override
+      public void afterCommit( TransactionData data, Object state ) {
+        System.out.println( "com.cedarsoft.serialization.neo4j.BasicNeo4JTest.afterCommit" );
+        System.out.println( data );
+      }
+
+      @Override
+      public void afterRollback( TransactionData data, Object state ) {
+        System.out.println( "com.cedarsoft.serialization.neo4j.BasicNeo4JTest.afterRollback" );
+        System.out.println( data );
+      }
+    } );
+
+    try ( Transaction tx = graphDb.beginTx() ) {
+      Index<Node> index = graphDb.index().forNodes( "myIndex" );
+
+      Node node = graphDb.createNode();
+      assertThat( node.getRelationships() ).hasSize( 0 );
+      assertThat( node ).isNotNull();
+
+      index.add( node, "myKey", 1 );
+
+      Relationship relationship = node.createRelationshipTo( node, Relations.MARRIED );
+      graphDb.index().forRelationships( "rels" ).add( relationship, "myKey", 7 );
+
+      tx.success();
+    }
+
+    try ( Transaction tx = graphDb.beginTx() ) {
+      Index<Node> index = graphDb.index().forNodes( "myIndex" );
+
+      assertThat( index.getEntityType() ).isEqualTo( Node.class );
+      assertThat( index.getName() ).isEqualTo( "myIndex" );
+
+      assertThat( ( Iterable<?> ) index.get( "myKey", 1 ) ).hasSize( 1 );
+      assertThat( index.get( "myKey", 1 ).getSingle() ).isNotNull();
+      assertThat( ( Iterable<?> ) index.query( "myKey", 1 ) ).hasSize( 1 );
+      assertThat( ( Iterable<?> ) index.get( "myKey", 0 ) ).hasSize( 0 );
+      assertThat( ( Iterable<?> ) index.query( "myKey", 0 ) ).hasSize( 0 );
+
+      assertThat( ( Iterable<?> ) index.query( "myKey:\"1\"" ) ).hasSize( 1 );
+      assertThat( ( Iterable<?> ) index.query( "myKey:\"0\"" ) ).hasSize( 0 );
+    }
+  }
+
+  @Test
+  public void testIndexDuplicates() throws Exception {
+    try ( Transaction tx = graphDb.beginTx() ) {
+      Index<Node> index = graphDb.index().forNodes( "myIndex" );
+
+      index.add( graphDb.createNode(), "myKey", 1 );
+      index.add( graphDb.createNode(), "myKey", 1 );
+
+      tx.success();
+    }
+
+    try ( Transaction tx = graphDb.beginTx() ) {
+      Index<Node> index = graphDb.index().forNodes( "myIndex" );
+      assertThat( index.getEntityType() ).isEqualTo( Node.class );
+      assertThat( index.getName() ).isEqualTo( "myIndex" );
+
+      assertThat( ( Iterable<?> ) index.query( "myKey", 1 ) ).hasSize( 2 );
+      assertThat( ( Iterable<?> ) index.query( "myKey", 0 ) ).hasSize( 0 );
+    }
+  }
+
+  @Test
+  public void testSelfRef() throws Exception {
+    try ( Transaction tx = graphDb.beginTx() ) {
+      Node node = graphDb.createNode();
+      assertThat( node.getRelationships() ).hasSize( 0 );
+      assertThat( node ).isNotNull();
+
+      Relationship relationship = node.createRelationshipTo( node, Relations.SON );
+      assertThat( relationship.getStartNode() ).isEqualTo( relationship.getEndNode() );
+
+      assertThat( node.getRelationships() ).hasSize( 1 );
+
+      tx.success();
+    }
+  }
+
   @Test
   public void testReferenceNode() throws Exception {
     try ( Transaction tx = graphDb.beginTx() ) {
---
+++ neo4j/src/test/java/com/cedarsoft/serialization/neo4j/CyperTest.java
@@ -0,0 +1,59 @@
+package com.cedarsoft.serialization.neo4j;
+
+import org.junit.*;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.ResourceIterator;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.helpers.collection.IteratorUtil;
+
+import java.util.Map;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class CyperTest extends AbstractNeo4JTest {
+
+
+  @Test
+  public void testCyper1() throws Exception {
+    ExecutionEngine engine = new ExecutionEngine( graphDb );
+
+
+    try ( Transaction tx = graphDb.beginTx() ) {
+      Node node1 = graphDb.createNode();
+      node1.setProperty( "name", "MM" );
+
+      Node node2 = graphDb.createNode();
+      Node node3 = graphDb.createNode();
+      Node node4 = graphDb.createNode();
+
+      node1.createRelationshipTo( node2, AbstractNeo4jSerializerTest.PersonSerializer.Relations.ADDRESS );
+      node1.createRelationshipTo( node3, AbstractNeo4jSerializerTest.PersonSerializer.Relations.ADDRESS );
+      node2.createRelationshipTo( node3, AbstractNeo4jSerializerTest.PersonSerializer.Relations.ADDRESS );
+      node3.createRelationshipTo( node4, AbstractNeo4jSerializerTest.PersonSerializer.Relations.ADDRESS );
+
+      tx.success();
+    }
+
+    String query = "start n=node(*) where n.name = 'MM' return n, n.name";
+    assertThat( engine.execute( query ).dumpToString().trim() ).isEqualTo( "+-----------------------------+\n" +
+                                                            "| n                  | n.name |\n" +
+                                                            "+-----------------------------+\n" +
+                                                            "| Node[1]{name:\"MM\"} | \"MM\"   |\n" +
+                                                            "+-----------------------------+\n" +
+                                                            "1 row" );
+
+    assertThat( engine.execute( query ).columns() ).containsExactly( "n", "n.name" );
+
+    boolean called = false;
+    for ( Node node : IteratorUtil.asIterable( engine.execute( query ).<Node>columnAs( "n" ) ) ) {
+      assertThat( node.getProperty( "name" ) ).isEqualTo( "MM" );
+      called = true;
+    }
+    assertThat( called ).isTrue();
+  }
+}
