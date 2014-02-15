055f52cc14efd790c28ce3961c6f03a4e345ac82
Johannes Schneider <js@cedarsoft.com>
2014-02-15 16:45:02 +0100

    added number serializer

---
+++ stax-mate/src/main/java/com/cedarsoft/serialization/stax/mate/primitives/NumberSerializer.java
@@ -0,0 +1,32 @@
+package com.cedarsoft.serialization.stax.mate.primitives;
+
+import com.cedarsoft.serialization.stax.mate.AbstractStaxMateSerializer;
+import com.cedarsoft.version.Version;
+import com.cedarsoft.version.VersionException;
+import com.cedarsoft.version.VersionRange;
+import org.codehaus.staxmate.out.SMOutputElement;
+
+import javax.annotation.Nonnull;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.IOException;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class NumberSerializer extends AbstractStaxMateSerializer<Number> {
+  public NumberSerializer() {
+    super( "number", "http://cedarsoft.com/primitives", VersionRange.single( 1, 0, 0 ) );
+  }
+
+  @Override
+  public void serialize( @Nonnull SMOutputElement serializeTo, @Nonnull Number object, @Nonnull Version formatVersion ) throws IOException, VersionException, XMLStreamException {
+    serializeTo.addCharacters( String.valueOf( object ) );
+  }
+
+  @Nonnull
+  @Override
+  public Number deserialize( @Nonnull XMLStreamReader deserializeFrom, @Nonnull Version formatVersion ) throws IOException, VersionException, XMLStreamException {
+    return Double.parseDouble( getText( deserializeFrom ) );
+  }
+}
---
+++ stax-mate/src/test/java/com/cedarsoft/serialization/stax/mate/primitives/NumberSerializerTest.java
@@ -0,0 +1,68 @@
+package com.cedarsoft.serialization.stax.mate.primitives;
+
+import com.cedarsoft.serialization.StreamSerializer;
+import com.cedarsoft.serialization.test.utils.AbstractXmlSerializerTest2;
+import com.cedarsoft.serialization.test.utils.Entry;
+import org.junit.*;
+import org.junit.experimental.theories.*;
+
+import javax.annotation.Nonnull;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.fail;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class NumberSerializerTest extends AbstractXmlSerializerTest2<Number> {
+  @Nonnull
+  @Override
+  protected StreamSerializer<Number> getSerializer() throws Exception {
+    return new NumberSerializer();
+  }
+
+  @Test
+  public void testNotClose() throws Exception {
+    final boolean[] shallAcceptClose = {false};
+
+    OutputStream out = new FilterOutputStream( new ByteArrayOutputStream() ) {
+      private boolean closed;
+
+      @Override
+      public void close() throws IOException {
+        if ( !shallAcceptClose[0] ) {
+          fail( "Unacceptable close!" );
+        }
+
+        super.close();
+        closed = true;
+      }
+    };
+
+    getSerializer().serialize( 123.0, out );
+    shallAcceptClose[0] = true;
+    out.close();
+  }
+
+  @Override
+  protected void verifyDeserialized( @Nonnull Number deserialized, @Nonnull Number original ) {
+    assertThat( deserialized.doubleValue() ).isEqualTo( original.doubleValue() );
+    assertThat( deserialized.intValue() ).isEqualTo( original.intValue() );
+  }
+
+  @DataPoint
+  public static final Entry<?> ENTRY1 = create( 123, "<number>123</number>" );
+  @DataPoint
+  public static final Entry<?> ENTRY2 = create( 123.5, "<number>123.5</number>" );
+  @DataPoint
+  public static final Entry<?> ENTRY3 = create( -123.5, "<number>-123.5</number>" );
+
+  @DataPoint
+  public static final Entry<?> ENTRY4 = create( Double.MAX_VALUE, "<number>1.7976931348623157E308</number>" );
+  @DataPoint
+  public static final Entry<?> ENTRY5 = create( -Double.MAX_VALUE, "<number>-1.7976931348623157E308</number>" );
+}
