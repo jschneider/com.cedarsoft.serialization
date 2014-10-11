0ddcbd6130d774668557f9a388a2d4373469cf73
Johannes Schneider <js@cedarsoft.com>
2014-10-11 15:06:30 +0200

    marked old method as deprecated

--- jackson/src/main/java/com/cedarsoft/serialization/jackson/AbstractJacksonSerializer.java
+++ jackson/src/main/java/com/cedarsoft/serialization/jackson/AbstractJacksonSerializer.java
@@ -361,6 +361,7 @@ public abstract class AbstractJacksonSerializer<T> extends AbstractStreamSeriali
     serializeTo.writeStringField( propertyName, enumValue.name() );
   }
 
+  @Deprecated
   @Nonnull
   public <T extends Enum<T>> T deserializeEnum( @Nonnull Class<T> enumClass, @Nonnull String propertyName, @Nonnull JacksonParserWrapper parser ) throws IOException {
     parser.nextFieldValue( propertyName );
