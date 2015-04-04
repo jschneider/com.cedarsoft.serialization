2715d92505359f671a86863dcdba7d48c0381a04
Johannes Schneider <js@cedarsoft.com>
2015-04-04 18:49:04 +0200

    replaced IAE with SerializationException

--- jackson/src/main/java/com/cedarsoft/serialization/jackson/JacksonParserWrapper.java
+++ jackson/src/main/java/com/cedarsoft/serialization/jackson/JacksonParserWrapper.java
@@ -30,6 +30,7 @@
  */
 package com.cedarsoft.serialization.jackson;
 
+import com.cedarsoft.serialization.SerializationException;
 import com.fasterxml.jackson.core.Base64Variant;
 import com.fasterxml.jackson.core.FormatSchema;
 import com.fasterxml.jackson.core.JsonLocation;
@@ -378,7 +379,7 @@ public class JacksonParserWrapper {
    */
   public void verifyDeserialized( @Nullable Object deserializedValue, @Nonnull String propertyName ) {
     if ( deserializedValue == null ) {
-      throw new IllegalStateException( "The field <" + propertyName + "> has not been deserialized" );
+      throw new SerializationException( SerializationException.Details.PROPERTY_NOT_DESERIALIZED, propertyName );
     }
   }
 }
