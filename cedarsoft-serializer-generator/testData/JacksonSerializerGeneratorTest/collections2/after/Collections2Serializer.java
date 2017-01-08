6ec03f887434b92f217640bb3ff7e9f1197dafbb
Johannes Schneider <js@cedarsoft.com>
2017-01-08 08:35:29 +0100

    add test for bug

--- cedarsoft-serializer-generator/testData/JacksonSerializerGeneratorTest/collections2/after/Collections2Serializer.java
+++ cedarsoft-serializer-generator/testData/JacksonSerializerGeneratorTest/collections2/after/Collections2Serializer.java
@@ -4,22 +4,30 @@ import java.io.IOException;
 import java.util.List;
 
 public class Collections2Serializer extends com.cedarsoft.serialization.jackson.AbstractJacksonSerializer<Collections2> {
-    public static final String PROPERTY_FOO_1 = "foo1";
-    public static final String PROPERTY_FOO_2 = "foo2";
+    public static final String PROPERTY_NAME = "name";
+    public static final String PROPERTY_ROLES = "roles";
+    public static final String PROPERTY_EMAILS = "emails";
+    public static final String PROPERTY_USER_DETAILS = "userDetails";
+    public static final String PROPERTY_SINGLE_EMAIL = "singleEmail";
 
     @javax.inject.Inject
-    public Collections2Serializer(@NotNull StringSerializer stringSerializer, @NotNull IntegerSerializer integerSerializer) {
+    public Collections2Serializer(@NotNull StringSerializer stringSerializer, @NotNull RoleSerializer roleSerializer, @NotNull EmailSerializer emailSerializer, @NotNull UserDetailsSerializer userDetailsSerializer) {
         super("collections-2", com.cedarsoft.version.VersionRange.from(1, 0, 0).to());
         getDelegatesMappings().add(stringSerializer).responsibleFor(String.class).map(1, 0, 0).toDelegateVersion(1, 0, 0);
-        getDelegatesMappings().add(integerSerializer).responsibleFor(Integer.class).map(1, 0, 0).toDelegateVersion(1, 0, 0);
+        getDelegatesMappings().add(roleSerializer).responsibleFor(Role.class).map(1, 0, 0).toDelegateVersion(1, 0, 0);
+        getDelegatesMappings().add(emailSerializer).responsibleFor(Email.class).map(1, 0, 0).toDelegateVersion(1, 0, 0);
+        getDelegatesMappings().add(userDetailsSerializer).responsibleFor(UserDetails.class).map(1, 0, 0).toDelegateVersion(1, 0, 0);
         assert getDelegatesMappings().verify();
     }
 
     @Override
     public void serialize(@NotNull com.fasterxml.jackson.core.JsonGenerator serializeTo, @NotNull Collections2 object, @NotNull com.cedarsoft.version.Version formatVersion) throws IOException, com.cedarsoft.version.VersionException, com.fasterxml.jackson.core.JsonProcessingException {
         verifyVersionWritable(formatVersion);
-        serializeArray(object.getFoo1(), String.class, PROPERTY_FOO_1, serializeTo, formatVersion);
-        serializeArray(object.getFoo2(), Integer.class, PROPERTY_FOO_2, serializeTo, formatVersion);
+        serialize(object.getName(), String.class, PROPERTY_NAME, serializeTo, formatVersion);
+        serializeArray(object.getRoles(), Role.class, PROPERTY_ROLES, serializeTo, formatVersion);
+        serializeArray(object.getEmails(), Email.class, PROPERTY_EMAILS, serializeTo, formatVersion);
+        serialize(object.getUserDetails(), UserDetails.class, PROPERTY_USER_DETAILS, serializeTo, formatVersion);
+        serialize(object.getSingleEmail(), Email.class, PROPERTY_SINGLE_EMAIL, serializeTo, formatVersion);
     }
 
     @Override
@@ -27,36 +35,60 @@ public class Collections2Serializer extends com.cedarsoft.serialization.jackson.
     public Collections2 deserialize(@NotNull com.fasterxml.jackson.core.JsonParser deserializeFrom, @NotNull com.cedarsoft.version.Version formatVersion) throws IOException, com.cedarsoft.version.VersionException, com.fasterxml.jackson.core.JsonProcessingException {
         verifyVersionWritable(formatVersion);
 
-        List<? extends String> foo1 = null;
-        List<? extends Integer> foo2 = null;
+        String name = null;
+        List<? extends Role> roles = null;
+        List<? extends Email> emails = null;
+        UserDetails userDetails = null;
+        Email singleEmail = null;
 
         com.cedarsoft.serialization.jackson.JacksonParserWrapper parser = new com.cedarsoft.serialization.jackson.JacksonParserWrapper(deserializeFrom);
         while (parser.nextToken() == com.fasterxml.jackson.core.JsonToken.FIELD_NAME) {
             String currentName = parser.getCurrentName();
 
-            if (currentName.equals(PROPERTY_FOO_1)) {
+            if (currentName.equals(PROPERTY_NAME)) {
                 parser.nextToken();
-                foo1 = deserializeArray(String.class, formatVersion, deserializeFrom);
+                name = deserialize(String.class, formatVersion, deserializeFrom);
                 continue;
             }
-            if (currentName.equals(PROPERTY_FOO_2)) {
+            if (currentName.equals(PROPERTY_ROLES)) {
                 parser.nextToken();
-                foo2 = deserializeArray(Integer.class, formatVersion, deserializeFrom);
+                roles = deserializeArray(Role.class, formatVersion, deserializeFrom);
+                continue;
+            }
+            if (currentName.equals(PROPERTY_EMAILS)) {
+                parser.nextToken();
+                emails = deserializeArray(Email.class, formatVersion, deserializeFrom);
+                continue;
+            }
+            if (currentName.equals(PROPERTY_USER_DETAILS)) {
+                parser.nextToken();
+                userDetails = deserialize(UserDetails.class, formatVersion, deserializeFrom);
+                continue;
+            }
+            if (currentName.equals(PROPERTY_SINGLE_EMAIL)) {
+                parser.nextToken();
+                singleEmail = deserialize(Email.class, formatVersion, deserializeFrom);
                 continue;
             }
             throw new IllegalStateException("Unexpected field reached <" + currentName + ">");
         }
 
-        parser.verifyDeserialized(foo1, PROPERTY_FOO_1);
-        assert foo1 != null;
-        parser.verifyDeserialized(foo2, PROPERTY_FOO_2);
-        assert foo2 != null;
+        parser.verifyDeserialized(name, PROPERTY_NAME);
+        assert name != null;
+        parser.verifyDeserialized(roles, PROPERTY_ROLES);
+        assert roles != null;
+        parser.verifyDeserialized(emails, PROPERTY_EMAILS);
+        assert emails != null;
+        parser.verifyDeserialized(userDetails, PROPERTY_USER_DETAILS);
+        assert userDetails != null;
+        parser.verifyDeserialized(singleEmail, PROPERTY_SINGLE_EMAIL);
+        assert singleEmail != null;
 
         parser.ensureObjectClosed();
 
-        Collections2 object = new Collections2();
-        object.setFoo1(foo1);
-        object.setFoo2(foo2);
+        Collections2 object = new Collections2(name, singleEmail, userDetails);
+        object.setRoles(roles);
+        object.setEmails(emails);
         return object;
     }
 }
